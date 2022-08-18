727d3fdf018a26020fb7caf7c52098309612df01
Andriy Bas <andriybas@Andriys-MacBook-Pro.local>
2022-08-18 18:45:44 +0300

    intercept requests

--- cypress/e2e/door/demo.cy.js
+++ cypress/e2e/door/demo.cy.js
@@ -3,6 +3,15 @@
 describe("Input test", () => {
   beforeEach(() => {
     cy.visit("https://doorstead-v3.webflow.io/");
+
+    // intercep regular requests, NOT selected
+    //   https://us-autocomplete-pro.api.smartystreets.com/lookup?&key=114303733343874592&search=960%20Pine%20St%20Apt&selected=
+    cy.intercept("GET", /\/lookup\?.*selected=$/i, { fixture: "smarty.json" });
+    // intercept selected requests (with "more entries")
+    //   https://us-autocomplete-pro.api.smartystreets.com/lookup?&key=114303733343874592&search=960%20Pine%20St%20Apt&selected=960%20Pine%20St%20Apt%20(12)%20San%20Francisco%20CA%2094108
+    cy.intercept("GET", /\/lookup\?.*selected=.+/i, {
+      fixture: "smarty_selected.json",
+    });
   });
 
   it("displays the placeholder by default", () => {
@@ -15,24 +24,19 @@ describe("Input test", () => {
 
   it("Should display multi-apartment address '960 pine str'", () => {
     cy.get("#autocomplete2").type("960 pine str");
-
     cy.get("ul.autocomplete-menu li div")
       .first()
       .invoke("text")
       .should("match", /\d+ more entries/i);
-
     cy.get("#autocomplete2+ul")
       .contains("li", "more entries")
       .click()
       .wait(1000);
-
     cy.get("ul.autocomplete-menu li div")
       .first()
       .invoke("text")
       .should("match", /960 Pine St Apt \d+ San Francisco/i);
-
     cy.get("#autocomplete2+ul li").first().click();
-
     cy.get("#autocomplete2")
       .invoke("val")
       .should("match", /960 Pine St Apt \d+ San Francisco/i);
